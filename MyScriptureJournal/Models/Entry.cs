8c8aa3eea4f417624338cca3bd6fa0ff09060bf6
Brienna Sambrano <brie2014@byui.edu>
2023-10-26 23:30:19 -0600

    Start of My Scripture Journal web app -CRUD -Searching

---
+++ MyScriptureJournal/Models/Entry.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyScriptureJournal.Models
+{
+    public class Entry
+    {
+        public int ID { get; set; }
+        public string? Book { get; set; }
+        public string? Reference { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+        public string? Note { get; set; }
+    }
+}
---
+++ MyScriptureJournal/Pages/Entries/Index.cshtml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MyScriptureJournal.Data;
+using MyScriptureJournal.Models;
+
+namespace MyScriptureJournal.Pages.Entries
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MyScriptureJournal.Data.MyScriptureJournalContext _context;
+
+        public IndexModel(MyScriptureJournal.Data.MyScriptureJournalContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Entry> Entry { get;set; } = default!;
+
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+
+        public SelectList? Books { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? Book { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            // Use LINQ to get list of genres.
+            IQueryable<string> bookQuery = from e in _context.Entry
+                                            orderby e.Book
+                                            select e.Book;
+            var entries = from e in _context.Entry
+                          select e;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                entries = entries.Where(s => s.Note.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(Book))
+            {
+                entries = entries.Where(x => x.Book == Book);
+            }
+            Books = new SelectList(await bookQuery.Distinct().ToListAsync());
+
+            Entry = await entries.ToListAsync();
+        }
+    }
+}
