5d4a264d62f706619d5af91ced83f8442972e71b
Brienna Sambrano <brie2014@byui.edu>
2023-10-28 14:32:54 -0600

    Finish My Scripture Journal Web App -filtering -UI

--- MyScriptureJournal/Pages/Entries/Index.cshtml.cs
+++ MyScriptureJournal/Pages/Entries/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using MyScriptureJournal.Data;
 using MyScriptureJournal.Models;
@@ -29,15 +30,20 @@ namespace MyScriptureJournal.Pages.Entries
 
         [BindProperty(SupportsGet = true)]
         public string? Book { get; set; }
+        public string DateSort { get; set; }
+        public string BookSort { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder)
         {
+            /* FILTERING */
             // Use LINQ to get list of genres.
             IQueryable<string> bookQuery = from e in _context.Entry
                                             orderby e.Book
                                             select e.Book;
             var entries = from e in _context.Entry
                           select e;
+
+            // Filter entries to match users search query
             if (!string.IsNullOrEmpty(SearchString))
             {
                 entries = entries.Where(s => s.Note.Contains(SearchString));
@@ -49,6 +55,28 @@ namespace MyScriptureJournal.Pages.Entries
             }
             Books = new SelectList(await bookQuery.Distinct().ToListAsync());
 
+            // If the sort order is empty, default it to nothing which will sort by date_desc (newest to oldest)
+            DateSort = String.IsNullOrEmpty(sortOrder) ? "date_asc" : "";
+            // If the sort order is book, switch it to book_desc
+            BookSort = sortOrder == "book" ? "book_desc" : "book";
+            // Sort entries in specific order
+            switch (sortOrder)
+            {
+                case "book_desc":
+                    entries = entries.OrderByDescending(e => e.Book);
+                    break;
+                case "book":
+                    entries = entries.OrderBy(e => e.Book);
+                    break;
+                case "date_asc":
+                    entries = entries.OrderBy(e => e.Date);
+                    break;
+                default:
+                    entries = entries.OrderByDescending(e => e.Date);
+                    break;
+            }
+
+            // Return filtered and sorted entries
             Entry = await entries.ToListAsync();
         }
     }
